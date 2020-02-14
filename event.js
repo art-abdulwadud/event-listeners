e13cb66a4db80d97e359b75a24f580fcbc22d844
maserati12 <triggamaserati@gmail.com>
2020-02-14 18:08:53 +0300

    click and submit events

--- event.js
+++ event.js
@@ -2,17 +2,22 @@
 const allImages = document.querySelectorAll('img');
 allImages.forEach(image => {
 	image.addEventListener('click', () => {
-		window.open(image.src)
+		window.open(image.src);
 	})
 })
 
 // Adding item to a list
-const list = document.querySelector('#books')
-const submit = document.querySelector('#submit')
-
-submit.addEventListener('click', (event) => {
-	event.preventDefault()
-	let newBook = document.createElement('li')
-	newBook.setAttribute('id', 'book')
-	list.appendChild(newBook)
-})
+const list = document.querySelector('#books');
+const bookEntry = document.querySelector('#book-entry');
+const submit = document.querySelector('#submit');
+const addBook = (event) => {
+	event.preventDefault();
+	let bookName = document.createTextNode(bookEntry.value);
+	// Check if input is not empty
+	if(bookName.length != 0){
+		let newBook = document.createElement('li');
+		newBook.setAttribute('id', 'book');
+		newBook.appendChild(bookName);
+		list.appendChild(newBook);
+	}
+}
